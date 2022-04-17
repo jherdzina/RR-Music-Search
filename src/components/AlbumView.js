f00ce0f2a95cff4b398df4b8334634bc6557ef99
Jennifer Herdzina <jen@Jennifers-MacBook-Pro.local>
2022-04-17 18:28:41 -0500

    commit assignment changes

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -1,18 +1,20 @@
 // These components will be making separate API calls from the app
 // component to serve specific data about a given album
+import React from 'react'
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 function AlbumView() {
     const { id } = useParams()
-    const [ albumData, setalbumData ] = useState([])
+    const [ albumData, setAlbumData ] = useState([])
 
     return (
         <div>
             <h2>The id passed was: {id}</h2>
-            <p>Album Data Goes Here!</p>
+            <p> {albumData} </p>
         </div>
     )
 }
+
 
 export default AlbumView
--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,5 +1,6 @@
 // These components will be making separate API calls from the app
 // component to serve specific data about our artist
+import React from 'react'
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
@@ -10,7 +11,7 @@ function ArtistView() {
     return (
         <div>
             <h2>The id passed was: {id}</h2>
-            <p>Artist Data Goes Here!</p>
+            <p> {artistData} </p>
         </div>
     )
 }
